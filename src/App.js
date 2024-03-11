737792c22afc7bbaaf5735bbd726d2855ef646b5
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-11 17:25:46 -0300

    criado alguns componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Container from "./components/Container";
+
 
 function App() {
   return (
     <>
     <Header />
-    <p>Conteudo</p>
+    <Banner image="home"/>
+    <Container>
+      <h2>Geografia</h2>
+  
+    </Container>
     <Footer />
     </>
   );
---
+++ src/components/Banner/index.js
@@ -0,0 +1,13 @@
+import styles from './Banner.module.css'
+
+function Banner({ image }){
+    return(
+        <div 
+            className={ styles.banner } 
+            style={ { backgroundImage: `url('/images/banner-${ image }.png')` } }>
+            
+        </div>
+    );
+}
+
+export default Banner;
