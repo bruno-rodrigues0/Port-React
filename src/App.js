83a8ace7143c8c75ca64868f0fd00d37be6f38f2
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-11 18:04:34 -0300

    aa

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
-
+import Card from "./components/Card";
+import videos from './json/db.json'
 
 function App() {
   return (
     <>
-    <Header />
-    <Banner image="home"/>
-    <Container>
-      <h2>Geografia</h2>
-  
-    </Container>
-    <Footer />
+      <Header />
+      <Banner image="home" />
+      <Container>
+          <h2>Geografia</h2>
+        <section className="cards">
+          {videos.map((video) => <Card id={video.id} key={video.id}/>)}
+        </section>
+          <h2>História</h2>
+        <section className="cards">
+          {videos.map((video) => <Card id={video.id} key={video.id}/>)}
+        </section>
+      </Container>
+      <Footer />
     </>
   );
 }
